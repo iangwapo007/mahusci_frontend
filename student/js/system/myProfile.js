0b044871ff3c2b35299a0fe78725e939d16021c6
iangwapo007 <iegeneralao@gmail.com>
2025-04-20 16:01:27 +0800

    added logout functionality(web view only)

--- student/js/system/myProfile.js
+++ student/js/system/myProfile.js
@@ -1,3 +1,26 @@
-import { setRouter } from "../utils/utils.js";
+import { setRouter, backendURL } from "../utils/utils.js";
 
 setRouter();
+
+const btn_logout = document.getElementById("btn_logout");
+
+btn_logout.onclick = async () => {
+  const response = await fetch(backendURL + "/api/logout", {
+    headers: {
+      Accept: "application/json",
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
+  });
+
+  if (response.status === 200) {
+    localStorage.clear();
+
+    const json = await response.json();
+
+    window.location.pathname = "/";
+  } else {
+    const json = await response.json();
+
+    alert(json.message);
+  }
+};
