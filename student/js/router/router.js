880f42080243e06fc0ed8e7b127cc5baffd3824e
Ashina22 <Palmerston03@gmail.com>
2025-04-21 04:16:37 +0800

    bushet wanaapil ang router

--- student/js/router/router.js
+++ student/js/router/router.js
@@ -1,25 +1,32 @@
-// function setRouter() {
-//   switch (window.location.pathname) {
-//     case "/":
-//     case "/index.html":
-//     case "/register.html":
-//       if (localStorage.getItem("token")) {
-//         window.location.pathname = "/student/home.html";
-//       }
-//       break;
-//     case "/student/home.html":
-//     case "/student/lecture.html":
-//     case "/student/lessons.html":
-//     case "/student/myLearnings.html":
-//     case "/student/myProfile.html":
-//     case "/student/quiz.html":
-//       if (!localStorage.getItem("token")) {
-//         window.location.pathname = "/index.html";
-//       }
-//       break;
-//     default:
-//       break;
-//   }
-// }
+const token = localStorage.getItem("token");
+const userType = localStorage.getItem("type")
+
+function setRouter() {
+  switch (window.location.pathname) {
+    case "/":
+    case "/index.html":
+    case "/register.html":
+      if (token) {
+        if(userType === "student"){
+        window.location.pathname = "/student/home.html";
+      }else{
+        window.location.pathname = "/teacher/home.html";
+      }
+      }
+      break;
+    case "/student/home.html":
+    case "/student/lecture.html":
+    case "/student/lessons.html":
+    case "/student/myLearnings.html":
+    case "/student/myProfile.html":
+    case "/student/quiz.html":
+      if (!token) {
+        window.location.pathname = "/index.html";
+      }
+      break;
+    default:
+      break;
+  }
+}
 
 export { setRouter };
